ec7ceea430495d22aedc82bcd935db81bfe8d50d
manuelcastiblan <mecastib@hotmail.com>
2024-02-19 18:22:33 -0500

    first commit

---
+++ target/debug/deps/learning_rust.d
@@ -0,0 +1,5 @@
+C:\Users\mecas\RustroverProjects\learning_rust\target\debug\deps\learning_rust.exe: src\main.rs
+
+C:\Users\mecas\RustroverProjects\learning_rust\target\debug\deps\learning_rust.d: src\main.rs
+
+src\main.rs:
---
+++ target/debug/learning_rust.d
@@ -0,0 +1,1 @@
+C:\Users\mecas\RustroverProjects\learning_rust\target\debug\learning_rust.exe: C:\Users\mecas\RustroverProjects\learning_rust\src\main.rs
